9fc401789cbf80e496ba1aee7eb75bc4b4a05380
unknown <bmbrunomonteiro69@gmail.com>
2019-11-06 15:45:43 -0300

    Definicao das variaveis

--- distancia.c
+++ distancia.c
@@ -6,4 +6,9 @@ float calculaDistancia (int x1, int y1, int x2, int y)
 	return sqrt( pow(x2 - x1, 2) + pow (y2 - y1, 2));
 }
 
-int main (){}
+int main ()
+{
+	int x1, y1, x2, y2;
+	float distancia;
+
+}
